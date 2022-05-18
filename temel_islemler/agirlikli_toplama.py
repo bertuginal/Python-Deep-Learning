eca2c303dfa9346b5b9a5ace1fa965d607da8920
Bertuğ İNAL <73167951+bertuginal@users.noreply.github.com>
2022-05-18 22:29:39 +0300

    Add files via upload

---
+++ temel_islemler/agirlikli_toplama.py
@@ -0,0 +1,21 @@
+# f(x,y) = x*a + y*b + c
+import cv2
+import numpy as np
+
+circle = np.zeros((512, 512, 3), np.uint8) + 255 #beyaz tuval
+cv2.circle(circle, (256, 256), 60, (255, 0, 0), -1) # merkeze bir içi dolu daire
+
+rectangle = np.zeros((512, 512, 3), np.uint8) + 255 #beyaz tuval
+cv2.rectangle(rectangle, (150, 150), (350, 350), (0, 0, 255), -1) # merkeze bir içi dolu dikdörtgen
+
+dst = cv2.addWeighted(circle, 0.9, rectangle, 0.1, 0) # hangi şeklinin renginin daha yoğun olacağını göstermek için kullanılır.
+
+
+cv2.imshow("Circle", circle)
+cv2.imshow("Rectangle", rectangle)
+cv2.imshow("Dst", dst)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
