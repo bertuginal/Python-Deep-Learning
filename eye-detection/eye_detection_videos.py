6be5a76b09817380784d7bc514839bfcf6d5f38f
Bertuğ İNAL <73167951+bertuginal@users.noreply.github.com>
2022-08-19 16:44:12 +0300

    Add files via upload

---
+++ eye-detection/eye_detection_videos.py
@@ -0,0 +1,32 @@
+import cv2
+
+vid = cv2.VideoCapture("eye.mp4")
+
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+eye_cascade = cv2.CascadeClassifier("eye.xml")
+
+while 1:
+    ret, frame = vid.read()
+    frame = cv2.resize(frame, (480, 360))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_frame = frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex, ey, ew, eh) in eyes:
+	        cv2.rectangle(roi_frame,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+
+    cv2.imshow('Eyes Capture', frame)
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
